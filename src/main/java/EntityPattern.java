47a311906f6d33d9e944aca5f033eabc07d73e68
Fajar <fajar.ekaputra@tuwien.ac.at>
2019-10-25 12:41:33 +0200

    add information related to RDF representation

--- src/main/java/EntityPattern.java
+++ src/main/java/EntityPattern.java
@@ -1,17 +1,22 @@
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.HashMap;
 import java.util.regex.Pattern;
-import java.util.List;
 
+/**
+ * Class representation of EntityPattern
+ */
 public class EntityPattern {
-    public Pattern pattern;
+
     public String className;
+    public String propertyName;
+    public Boolean isObject;
+    public Pattern pattern;
     public EnumPatternType type;
 
-    public EntityPattern(String className, Pattern pattern, EnumPatternType type){
+    public EntityPattern(String className, String propertyName, Boolean isObject, Pattern pattern,
+            EnumPatternType type) {
         this.className = className;
+        this.propertyName = propertyName;
         this.pattern = pattern;
+        this.isObject = isObject;
         this.type = type;
     }
 }
