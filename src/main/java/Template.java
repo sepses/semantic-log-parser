59377758d80155d8d35eeed814e058158215239d
Fajar <fajar.ekaputra@tuwien.ac.at>
2019-10-25 12:41:09 +0200

    change parameterDict type to EntityPattern

--- src/main/java/Template.java
+++ src/main/java/Template.java
@@ -1,14 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class representation of a template
+ */
 public class Template {
+
     public String TemplateId;
     public String TemplateContent;
-    public List<String> parameterDict;
+    public List<EntityPattern> parameterDict;
 
-    public Template(String TemplateId, String TemplateContent){
+    public Template(String TemplateId, String TemplateContent) {
         this.TemplateId = TemplateId;
         this.TemplateContent = TemplateContent;
-        parameterDict = new ArrayList<String>();
+        parameterDict = new ArrayList<>();
     }
 }
